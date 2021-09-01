77c08893b6e58a3a05adc8596c7c7b20656d222c
phil.yeung <philsdevstuff@gmail.com>
2021-09-01 12:37:22 +0100

    Testing commit

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,14 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+    int balance;
+
+    public void deposit(int amount) {
+        this.balance = amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
 }
