52128d2aac08d3d1c9b248c25b0b82e456f99e28
phil.yeung <philsdevstuff@gmail.com>
2021-12-15 22:10:59 +0000

    Creating Money class

--- src/main/java/org/xpdojo/bank/Money.java
+++ src/main/java/org/xpdojo/bank/Money.java
@@ -1,4 +1,33 @@
 package org.xpdojo.bank;
 
+import java.util.Objects;
+
 public class Money {
+    private final double amount;
+
+    private Money(double amount) {
+        this.amount = amount;
+    }
+
+    public static Money amountOf(double amount) {
+        return new Money(amount);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return Double.compare(money.amount, amount) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Money = : " + amount;
+    }
 }
--- src/test/java/org/xpdojo/bank/unit/MoneyTest.java
+++ src/test/java/org/xpdojo/bank/unit/MoneyTest.java
@@ -2,11 +2,21 @@ package org.xpdojo.bank.unit;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.xpdojo.bank.Account.*;
+import static org.xpdojo.bank.Money.amountOf;
 
 public class MoneyTest {
 
+    @Test
+    public void canMakeTheMoneys() {
+        var money = amountOf(15);
+
+        assertEquals(money, amountOf(15));
+    }
 }
